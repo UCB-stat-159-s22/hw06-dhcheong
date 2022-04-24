f158f2d1ace2df44c063b1145a409fecbc1305cb
Dae Hyeun Cheong <dhcheong@berkeley.edu>
2022-04-24 00:17:24 +0000

    added some test files to tests folder

--- ligotools/tests/tests_readligo.py
+++ ligotools/tests/tests_readligo.py
@@ -1,17 +1,21 @@
-from ligotools import readligo as rl
-import h5py
+import ligotools.readligo as rl
+import numpy as np
 
-def test_dimension_H1():
+def test_loaddata():
 	strain_H1, time_H1, chan_dict_H1 = rl.loaddata("data/H-H1_LOSC_4_V2-1126259446-32.hdf5", 'H1')
 	assert (len(strain_H1) == 131072) & (len(time_H1) == 131072) & (len(chan_dict_H1) == 13)
 
-def test_dimension_L1():
-	strain_L1, time_L1, chan_dict_L1 = rl.loaddata("data/L-L1_LOSC_4_V2-1126259446-32.hdf5", 'L1')
-	assert (len(strain_L1) == 131072) & (len(time_L1) == 131072) & (len(chan_dict_L1) == 13)
-
-def test_F_template():
-	f_template = h5py.File('data/GW150914_4_template.hdf5', "r")
-	assert (f_template is not None)
+def test_dq_channel_to_seglist():
+	c = np.array([1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1])
+	assert np.array_equal(rl.dq_channel_to_seglist(c),[slice(0, 131072, None)])
 
 def test_read_hdf5():
-	assert rl.read_hdf5('data/H-H1_LOSC_4_V2-1126259446-32.hdf5', readstrain=True)[2] == 0.000244140625
+	assert rl.read_hdf5('data/H-H1_LOSC_4_V2-1126259446-32.hdf5', readstrain=True)[1] == 1126259446
+
+def test_FileList_searchdir():
+	hdf5_files = rl.FileList().searchdir('ligotools/')
+	assert np.array_equal(hdf5_files, [])
+
+
+
+
---
+++ ligotools/tests/tests_utils.py
@@ -0,0 +1,13 @@
+from ligotools.utils import *
+
+def test_whiten():
+  return
+
+def test_write_wavefile():
+  return
+
+def test_reqshift():
+  return
+  
+def test_plot_changes():
+  return
